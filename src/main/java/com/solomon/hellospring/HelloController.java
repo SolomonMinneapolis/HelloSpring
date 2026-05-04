7a051155240cf97a493512a5fb1757c3c9497ec5
SolomonMinneapolis <solomon.witte@go.minneapolis.edu>
2026-05-03 22:05:55 -0500

    Added more variables to the hello template.

--- src/main/java/com/solomon/hellospring/HelloController.java
+++ src/main/java/com/solomon/hellospring/HelloController.java
@@ -12,6 +12,9 @@ public class HelloController {
 
         String name = "Solomon";
         model.addAttribute("name", name);
+        model.addAttribute("className", "Java");
+        model.addAttribute("classCode", 2545);
+
         return "hello";
     }
 
